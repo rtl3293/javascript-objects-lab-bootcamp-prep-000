76f70f33f09c0abdcf532c3b6e136fb53960200c
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 17:47:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-  var updatedObj = Object.assign(obj, key, value)
+  var updatedObj = Object.assign(obj, [key: value])
   return updatedObj
 }
