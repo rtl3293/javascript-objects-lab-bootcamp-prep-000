4034f1ca8e216a3393fe46bc2223b239dad69c0b
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 17:57:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   obj[key] = value
   return obj
 }
+
+function deleteFromObjectByKey(obj, key) {
+  newObj = Object.assign({}, obj)
+  delete newObj[key]
+  return newObj
+}
